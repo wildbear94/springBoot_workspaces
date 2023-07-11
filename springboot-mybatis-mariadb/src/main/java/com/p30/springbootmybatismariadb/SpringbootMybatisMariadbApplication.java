c1cd3327bee9f10a6dde878018b674a055a5370e
park sang young <wildbear94@gmail.com>
2023-07-11 09:52:39 +0900

    마이바티스 마리아 디비 연동 프로젝트 소스 추가

---
+++ springboot-mybatis-mariadb/src/main/java/com/p30/springbootmybatismariadb/SpringbootMybatisMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.p30.springbootmybatismariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisMariadbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootMybatisMariadbApplication.class, args);
+  }
+
+}
---
+++ springboot-mybatis-mariadb/src/test/java/com/p30/springbootmybatismariadb/SpringbootMybatisMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p30.springbootmybatismariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisMariadbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
